450752640ed93a2fb72cd528b585b4056367decf
alexa <allexa.msv@gmail.com>
2023-08-18 01:47:05 +0300

    objects

--- Rick&Morty/MVVM/Model.swift
+++ Rick&Morty/MVVM/Model.swift
@@ -6,3 +6,64 @@
 //
 
 import Foundation
+
+struct Personage {
+    let id: Int
+    let name: String
+    let image: String
+    let status: Status
+    let species: Species
+    let type: String
+    let gender: Gender
+    let origin: Origin
+    let episodes: [Episode]
+}
+
+struct Origin {
+    let id: Int
+    let name: String
+    let type: String
+}
+
+struct Episode {
+    let id: Int
+    let name: String
+    let airDate: String
+    let episodeCode: String
+}
+
+// MARK: enums
+
+enum Status: String {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
+
+enum Species: String {
+    case human = "Human"
+    case alien = "Alien"
+}
+
+enum Gender: String {
+    case male = "Male"
+    case female = "Female"
+}
+
+// MARK: Keys
+
+enum Keys {
+    case results
+    case id
+    case name
+    case image
+    case status
+    case species
+    case type
+    case gender
+    case origin
+    case episodes
+    case episode
+    case residents
+    case air_date
+}
