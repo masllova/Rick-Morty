7883655ff6b900ca55441150c022a5116f6a4ece
alexa <allexa.msv@gmail.com>
2023-08-17 23:43:54 +0300

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Александра Маслова on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
